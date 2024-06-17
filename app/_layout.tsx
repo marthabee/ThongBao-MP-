c22f69d12a545af3ac655142d9fdd04c21ff253e
Hai Nguyen Thi <BIT220048@st.cmc-u.edu.vn>
2024-06-17 20:55:45 +0700

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,131 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import React from 'react';
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+const data = [
+  {
+    id: 1,
+    image: 'https://bootdey.com/img/Content/avatar/avatar1.png',
+    name: 'Frank Odalthh',
+    comment: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+  },
+  {
+    id: 2,
+    image: 'https://bootdey.com/img/Content/avatar/avatar6.png',
+    name: 'John DoeLink',
+    comment: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+  },
+  {
+    id: 3,
+    image: 'https://bootdey.com/img/Content/avatar/avatar7.png',
+    name: 'March SoulLaComa',
+    comment: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+  },
+  {
+    id: 4,
+    image: 'https://bootdey.com/img/Content/avatar/avatar2.png',
+    name: 'Finn DoRemiFaso',
+    comment: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+  },
+  {
+    id: 5,
+    image: 'https://bootdey.com/img/Content/avatar/avatar3.png',
+    name: 'Maria More More',
+    comment: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+  },
+  {
+    id: 6,
+    image: 'https://bootdey.com/img/Content/avatar/avatar4.png',
+    name: 'Clark June Boom!',
+    comment: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+  },
+  {
+    id: 7,
+    image: 'https://bootdey.com/img/Content/avatar/avatar5.png',
+    name: 'The googler',
+    comment: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.',
+  },
+];
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <View style={styles.root}>
+      <Text style={styles.headerText}>Thông báo</Text>
+      <FlatList
+        data={data}
+        extraData={data}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.container}>
+              <TouchableOpacity onPress={() => {}}>
+                <Image style={styles.image} source={{ uri: item.image }} />
+              </TouchableOpacity>
+              <View style={styles.content}>
+                <View style={styles.contentHeader}>
+                  <Text style={styles.name}>{item.name}</Text>
+                  <Text style={styles.time}>9:58 am</Text>
+                </View>
+                <Text style={styles.comment}>{item.comment}</Text>
+              </View>
+            </View>
+          );
+        }}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    backgroundColor: '#ffffff',
+    marginTop: 50,
+    flex: 1,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  container: {
+    paddingLeft: 19,
+    paddingRight: 16,
+    paddingVertical: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  content: {
+    marginLeft: 16,
+    flex: 1,
+  },
+  contentHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 6,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#CCCCCC',
+  },
+  image: {
+    width: 45,
+    height: 45,
+    borderRadius: 22.5,
+    marginLeft: 20,
+  },
+  time: {
+    fontSize: 11,
+    color: '#808080',
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  comment: {
+    fontSize: 14,
+    color: '#333333',
+  },
+});
